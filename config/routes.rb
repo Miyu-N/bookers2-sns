f1bddd9a168225bf0b725ae5a9399e0005cfb2c2
Miyu-N <myu.star1217@gmail.com>
2021-06-03 13:00:23 +0000

    検索機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-  
-  get 'relationships/create'
-  get 'relationships/destroy'
   devise_for :users
-   root to: 'homes#top'
-   get 'home/about' => 'homes#about'
-   
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+  root 'homes#top'
+  resources :users, only: [:index, :show, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  get 'home/about' => 'homes#about'
   
-  resources :users, only: [:index, :show, :edit, :update] do
-    resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
-  end
+  get 'search' => 'searches#search'
 end
